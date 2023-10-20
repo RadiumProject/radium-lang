c080b96930cd807873d21f3584bd7a340b404059
setscript <ponomarv-d@internet.ru>
2023-10-20 15:34:20 +0300

    update rpu

--- rpu/rpu.rb
+++ rpu/rpu.rb
@@ -6,13 +6,23 @@ subcommand = ARGV[1]
 
 commands = [
     "--new-project",
-    "--help"
+    "--help",
+    "--debug-deps",
+    "--start"
 ]
 
 if command == commands[0]
-    system("sh structgen.sh #{subcommand}")
+    system("sh src/structgen.sh #{subcommand}")
 elsif command == commands[1]
-    puts "soon.."
+    puts "help for RPU(Radium Packages Utils):
+    --new-project: create new radium project.
+    --help: output this text.
+    --debug-deps: check the deps for project.
+    --start: start project."
+elsif command == commands[2]
+    system("sh src/depsdebug.sh")
+elsif command == commands[3]
+    system("sh src/projectstart.sh")
 else
     puts "unkown command.\nwrite --help for commands list."
 end
